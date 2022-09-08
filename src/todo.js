b81c18030996556091dd0a1600c126fc89945034
Circlewee <pnj1118@naver.com>
2022-09-08 19:43:50 +0900

    feat: JSDoc의 optional property 적용

--- src/todo.js
+++ src/todo.js
@@ -38,7 +38,7 @@
  * Todo를 추가할 수 있다. (내용없이 추가할 수 없음)
  * @param {Content} content - Todo 내용
  * @param {Category} category - 카테고리
- * @param {Tag[] | undefined} tags - 태그 list
+ * @param {Tag[]} [tags] - 태그 list
  * @return {Todo} 생성된 Todo
  */
 function createTodo(content, category, tags) {}
@@ -46,7 +46,7 @@ function createTodo(content, category, tags) {}
 /**
  * 1. 모든 Todo를 조회할 수 있다.
  * 2. ID를 기반으로 특정 Todo를 조회할 수 있다.
- * @param {Id | undefined} id - 해당 todo id
+ * @param {Id} [id] - 해당 todo id
  * @return {Todo | Todo[]} id에 해당하는 Todo, 혹은 모든 Todo
  */
 function readTodo(id) {}
@@ -56,8 +56,8 @@ function readTodo(id) {}
  * 2. 모든 Todo을 제거할 수 있다.
  * 3. 특정 Todo의 특정 태그를 삭제할 수 있다.
  * 4. 특정 Todo의 모든 태그를 제거할 수 있다.
- * @param {Id | undefined} id - 해당 todo id
- * @param {Tag | undefined} tag - 삭제하고 싶은 tag
+ * @param {Id} [id] - 해당 todo id
+ * @param {Tag | undefined} [tag] - 삭제하고 싶은 tag
  */
 function deleteTodo(id, tag) {}
 
@@ -65,10 +65,10 @@ function deleteTodo(id, tag) {}
  * 1. ID를 제외한 모든 속성을 수정할 수 있다.
  * 2. 특정 Todo의 특정 태그를 수정할 수 있다.
  * @param {Id} id
- * @param {Content | undefined} content - Todo 내용
- * @param {IsComplete | undefined} isComplete - Todo 완료 여부
- * @param {Category | undefined} category - Todo 카테고리
- * @param {Tag | undefined} tag - Todo 태그 list
+ * @param {Content} [content] - Todo 내용
+ * @param {IsComplete} [isComplete] - Todo 완료 여부
+ * @param {Category} [category] - Todo 카테고리
+ * @param {Tag} [tag] - Todo 태그 list
  * @return {Todo} update된 Todo
  */
 function updateTodo(id, content, isComplete, category, tag) {}
