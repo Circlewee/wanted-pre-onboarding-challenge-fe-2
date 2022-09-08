536e6335925453d81078580c1dbf804f8f7a52a4
Circlewee <pnj1118@naver.com>
2022-09-08 20:21:47 +0900

    feat: todo에 api 적용

--- src/todo.js
+++ src/todo.js
@@ -1,22 +1,7 @@
-/**
- * CREATE
- * 할 일을 추가할 수 있다.
- * 내용없이 추가할 수 없다.
- *
- * READ
- * 모든 할 일을 조회할 수 있다.
- * ID를 기반으로 특정 할 일을 조회할 수 있다.
- *
- * UPDATE
- * ID를 제외한 모든 속성을 수정할 수 있다.
- * 특정 할 일의 특정 태그를 수정할 수 있다.
- *
- * DELETE
- * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * 모든 할 일을 제거할 수 있다.
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * 특정 할 일의 모든 태그를 제거할 수 있다.
- */
+import instance from './axiosInstance';
+import Api from './api';
+
+const api = new Api(instance);
 
 /** @typedef {string | number} Id */
 /** @typedef {string} Content */
@@ -41,7 +26,11 @@
  * @param {Tag[]} [tags] - 태그 list
  * @return {Todo} 생성된 Todo
  */
-function createTodo(content, category, tags) {}
+async function createTodo(content, category, tags) {
+  const response = await api.post('/post', { content, category, tags });
+
+  return response;
+}
 
 /**
  * 1. 모든 Todo를 조회할 수 있다.
@@ -49,7 +38,19 @@ function createTodo(content, category, tags) {}
  * @param {Id} [id] - 해당 todo id
  * @return {Todo | Todo[]} id에 해당하는 Todo, 혹은 모든 Todo
  */
-function readTodo(id) {}
+async function readTodo(id) {
+  let endpoint = '/read/';
+
+  if (id) {
+    endpoint += id;
+  } else {
+    endpoint += 'all';
+  }
+
+  const response = await api.get(endpoint);
+
+  return response;
+}
 
 /**
  * 1. ID를 기반으로 특정 Todo를 삭제할 수 있다.
@@ -57,9 +58,11 @@ function readTodo(id) {}
  * 3. 특정 Todo의 특정 태그를 삭제할 수 있다.
  * 4. 특정 Todo의 모든 태그를 제거할 수 있다.
  * @param {Id} [id] - 해당 todo id
- * @param {Tag | undefined} [tag] - 삭제하고 싶은 tag
+ * @param {Tag} [tag] - 삭제하고 싶은 tag
  */
-function deleteTodo(id, tag) {}
+async function deleteTodo(id, tag) {
+  await api.delete('/delete', { id, tag });
+}
 
 /**
  * 1. ID를 제외한 모든 속성을 수정할 수 있다.
@@ -71,4 +74,8 @@ function deleteTodo(id, tag) {}
  * @param {Tag} [tag] - Todo 태그 list
  * @return {Todo} update된 Todo
  */
-function updateTodo(id, content, isComplete, category, tag) {}
+async function updateTodo(id, content, isComplete, category, tag) {
+  const response = await api.update(`/update/${id}`, { content, isComplete, category, tag });
+
+  return response;
+}
